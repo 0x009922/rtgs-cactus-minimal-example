d6321276eb7e7a40d7d9c7eb1e5e3f3439dda6bb
0x009922 <a.marcius26@gmail.com>
2022-10-26 14:06:50 +0700

    init

---
+++ src/client.ts
@@ -0,0 +1,51 @@
+// const PORT =
+
+import cac from 'cac'
+import {
+  BlockTypeV1,
+  Iroha2ApiClient,
+  IrohaInstruction,
+  IrohaQuery,
+} from '@hyperledger/cactus-plugin-ledger-connector-iroha2'
+import { Configuration } from '@hyperledger/cactus-core-api'
+import { VersionedCommittedBlock } from '@iroha2/data-model'
+import { match, P } from 'ts-pattern'
+import consola from 'consola'
+
+const cli = cac('cactus-client')
+
+cli.command('[apiHost]').action(async ({ apiHost }) => {
+  const client = new Iroha2ApiClient(new Configuration({ basePath: apiHost }))
+
+  client.watchBlocksV1({ type: BlockTypeV1.Binary }).subscribe({
+    next: (event) => {
+      match(event)
+        .with({ binaryBlock: P.not(P.nullish) }, async ({ binaryBlock }) => {
+          const block = VersionedCommittedBlock.fromBuffer(
+            Buffer.from(binaryBlock)
+          )
+
+          // ! ADD YOUR BLOCK HANDLING LOGIC HERE
+
+          // you can make a transaction:
+          await client.transactV1({
+            transaction: {
+              instruction: { name: IrohaInstruction.MintAsset, params: [] },
+            },
+          })
+
+          // and a query:
+          await client.queryV1({
+            query: {
+              query: IrohaQuery.FindAllDomains,
+            },
+          })
+        })
+        .otherwise((x) => {
+          consola.error('Cannot parse block: %o', x)
+        })
+    },
+  })
+})
+
+cli.runMatchedCommand()
---
+++ src/node.ts
@@ -0,0 +1,74 @@
+import {
+  LogLevelDesc,
+  IListenOptions,
+  Servers,
+} from '@hyperledger/cactus-common'
+import { PluginRegistry } from '@hyperledger/cactus-core'
+import { Constants } from '@hyperledger/cactus-core-api'
+
+import {
+  PluginLedgerConnectorIroha2,
+  Iroha2BaseConfig,
+} from '@hyperledger/cactus-plugin-ledger-connector-iroha2'
+
+import { crypto } from '@iroha2/crypto-target-node'
+import { setCrypto } from '@iroha2/client'
+
+import { v4 as uuid } from 'uuid'
+import { Server as SocketIoServer } from 'socket.io'
+import http from 'http'
+import express from 'express'
+import bodyParser from 'body-parser'
+import consola from 'consola'
+
+import CONFIG from '../config.json'
+
+const LOG_LEVEL: LogLevelDesc = 'info'
+
+setCrypto(crypto)
+
+async function start(config: Required<Iroha2BaseConfig>): Promise<{
+  stop: () => Promise<void>
+}> {
+  const connector = new PluginLedgerConnectorIroha2({
+    instanceId: uuid(),
+    pluginRegistry: new PluginRegistry({ plugins: [] }),
+    logLevel: LOG_LEVEL,
+    defaultConfig: config,
+  })
+
+  // Run http server
+  const expressApp = express()
+  expressApp.use(bodyParser.json({ limit: '250mb' }))
+  const httpServer = http.createServer(expressApp)
+  const listenOptions: IListenOptions = {
+    hostname: '127.0.0.1',
+    port: 0,
+    server: this.connectorServer,
+  }
+  const addressInfo = await Servers.listen(listenOptions)
+  const apiHost = `http://${addressInfo.address}:${addressInfo.port}`
+
+  const socketIoServer = new SocketIoServer(this.connectorServer, {
+    path: Constants.SocketIoConnectionPathV1,
+  })
+
+  await connector.getOrCreateWebServices()
+  await connector.registerWebServices(expressApp, this.socketioServer)
+
+  async function stop() {
+    await Promise.all([
+      new Promise<void>((resolve) => socketIoServer.close(() => resolve())),
+      new Promise<void>((resolve) => httpServer.close(() => resolve())),
+    ])
+  }
+
+  consola.info('Api host: %o', apiHost)
+
+  return { stop }
+}
+
+start(CONFIG).catch((err) => {
+  consola.fatal(err)
+  process.exit(1)
+})
